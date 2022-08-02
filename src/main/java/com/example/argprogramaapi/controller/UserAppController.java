96e3807d6cb07c78c891e364f6b0a847d8532e07
Emir Moretti <emir.moretti@hotmail.com.ar>
2022-08-02 15:37:30 -0300

    usercontroller and properties

--- src/main/java/com/example/argprogramaapi/controller/UserAppController.java
+++ src/main/java/com/example/argprogramaapi/controller/UserAppController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
 public class UserAppController {
@@ -18,6 +19,11 @@ public class UserAppController {
     public ResponseEntity<?> getUser(@PathVariable(name = "id") Long id){
         UserApp user = userAppService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody UserApp userApp){
+        UserApp userDb = userAppService.update(userApp);
+        return ResponseEntity.ok().body(userDb);
     }
     @PutMapping("/{id}")
     public ResponseEntity<?> update(@PathVariable(name="id") Long id, @RequestBody UserApp userApp){
