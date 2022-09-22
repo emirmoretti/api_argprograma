7133837c09f867d271c002bd6047d2cd248eae0f
Emir Moretti <emir.moretti@hotmail.com.ar>
2022-09-22 11:45:30 -0300

    skills

--- src/main/java/com/example/argprogramaapi/model/Skill.java
+++ src/main/java/com/example/argprogramaapi/model/Skill.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Skill {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
