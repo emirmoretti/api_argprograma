a26dd5894c77083b14a49b4d6de242b6c7ed7a49
Emir Moretti <emir.moretti@hotmail.com.ar>
2022-12-02 16:19:23 -0300

    application dev and prod

--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
--- INSERT INTO profile(name, last_name, title, description) values ("emir", "moretti",  "programador fullstack", "rosarino");
-
--- INSERT INTO education(name) values ("Instituto superior ISPC");
--- INSERT INTO skill(name) values ("java");
